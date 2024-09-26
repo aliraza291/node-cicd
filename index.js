8c459f19dc1ace40f6344ad6de61a9d76e3e1661
aliraza291 <alirazajutt5412@gmail.com>
2024-09-26 07:13:38 +0500

    hello

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route for the root URL ("/") that responds with "Hello, World!"
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello Xeven");
 });
 
 // Define the port to listen on
